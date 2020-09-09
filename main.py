89fca3bda04fafe5b86691cabacf5839adeae091
isaaclee12 <isaac.lee@uvm.edu>
2020-09-09 15:11:18 -0400

    Added some new wrapper functions, need some SQL stuff?

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ def main():
     #test
     userCommand = input("Enter a command: ")
 
+    interpretCommand(userCommand)
+
+def interpretCommand(userCommand):
+
+    #Pseudocode:
+    #Extract whatever the first word is of the command, split @ first space
+    #Use switch statement to choose which command, or SQL stuff?
+
 main()
