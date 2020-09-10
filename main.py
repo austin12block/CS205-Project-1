18cb405a1d5fbf042041c1b5882b0b545b17a0e6
Austin Block <austin.block@live.com>
2020-09-10 19:41:51 -0400

    Created loadDatatoCsv function.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, csv
 from sqlite3 import Error
 
 #SQL Instructions:
@@ -27,10 +27,46 @@ def createSQLConnection():
     except Error as error:
         print('Cannot connect to database. The following error occurred: ', error)
 
+def loadCSVtoDB():
+    try:
+        # Start DB connection
+        connection = sqlite3.connect('topSpotifySongs.db')
+        cursor = connection.cursor()
+        # Prep the database by cleaning it
+        cursor.executescript("""DROP TABLE IF EXISTS "TopSpotifySongs";
+                                DROP TABLE IF EXISTS "TopArtistData";
+                                CREATE TABLE "TopSpotifySongs" (pmk INTEGER PRIMARY KEY,
+                                                                song VARCHAR(64),
+                                                                artist VARCHAR(32),
+                                                                genre VARCHAR(32));
+                                CREATE TABLE "TopArtistData" (pmk INTEGER PRIMARY KEY,
+                                                                artist VARCHAR(32),
+                                                                birthdate VARCHAR(16),
+                                                                hometown VARCHAR(64));""")
+        # Load the data from 1st CSV into array
+        with open('table-1.csv', 'r') as csvFile1:
+            reader = csv.DictReader(csvFile1)
+            dbArray1 = [(cell['pmk'], cell['song'], cell['artist'], cell['genre']) for cell in reader]
+        # Load the data from 1st CSV into array
+        with open('table-2.csv', 'r') as csvFile2:
+            reader = csv.DictReader(csvFile2)
+            dbArray2 = [(cell['pmk'], cell['artist'], cell['birthdate'], cell['hometown']) for cell in reader]
+        # Dump arrays into database
+        cursor.executemany("INSERT INTO TopSpotifySongs (pmk, song, artist, genre) VALUES (?, ?, ?, ?);", dbArray1)
+        cursor.executemany("INSERT INTO TopArtistData (pmk, artist, birthdate, hometown) VALUES (?, ?, ?, ?);", dbArray2)
+        connection.commit()
+        # Close DB connection
+        connection.close()
+    except Error as error:
+        print('Cannot connect to database. The following error occurred: ', error)
+
+
 def main():
     # establish SQLite connection
-    createSQLConnection()
-    
+    # createSQLConnection()
+
+    loadCSVtoDB()
+
     #test
     userCommand = input("Enter a command: ")
 
