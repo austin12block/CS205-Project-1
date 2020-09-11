0a0437fd03638a7895ff65cc39cb166abfbb8cb9
Austin Block <austin.block@live.com>
2020-09-11 14:41:58 -0400

    Added new line stripping

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def loadCSVtoDB():
         # Load the data from 1st CSV into array
         with open('table-1.csv', 'r') as csvFile1:
             reader = csv.DictReader(csvFile1)
-            dbArray1 = [(cell['pmk'], cell['song'], cell['artist'], cell['genre']) for cell in reader]
+            dbArray1 = [(cell['pmk'].rstrip(), cell['song'].rstrip(), cell['artist'].rstrip(), cell['genre'].rstrip()) for cell in reader]
         # Load the data from 1st CSV into array
         with open('table-2.csv', 'r') as csvFile2:
             reader = csv.DictReader(csvFile2)
-            dbArray2 = [(cell['pmk'], cell['artist'], cell['birthdate'], cell['hometown']) for cell in reader]
+            dbArray2 = [(cell['pmk'].rstrip(), cell['artist'].rstrip(), cell['birthdate'].rstrip(), cell['hometown'].rstrip()) for cell in reader]
         # Dump arrays into database
         cursor.executemany("INSERT INTO TopSpotifySongs (pmk, song, artist, genre) VALUES (?, ?, ?, ?);", dbArray1)
         cursor.executemany("INSERT INTO TopArtistData (pmk, artist, birthdate, hometown) VALUES (?, ?, ?, ?);", dbArray2)
